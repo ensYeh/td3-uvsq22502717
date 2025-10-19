64f50a7837b308e3bf8b12cdd481f160575f0166
uvsq22502717 <volodymyr.grynenko@ens.uvsq.fr>
2025-10-19 23:32:54 +0200

    Dns class tested fixed temporary file

--- src/test/java/fr/uvsq/cprog/collex/DnsTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsTest.java
@@ -1,39 +1,60 @@
 package fr.uvsq.cprog.collex;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DnsTest {
 
-  private Path tempFile() {
-    // For tests utiliser null, 
-    return Paths.get("dns_test.txt");
+  private Path tempFile;
+  private Dns dns;
+
+  @Before
+  public void setup() throws Exception {
+    // Create temporary file DNS
+    tempFile = Files.createTempFile("dns_test", ".txt");
+    dns = new Dns(tempFile);
+  }
+
+  @After
+  public void teardown() throws Exception {
+    // Delete temporary file after the test
+    Files.deleteIfExists(tempFile);
   }
 
   @Test
   public void testAddAndRetrieveItem() throws Exception {
-    Dns dns = new Dns(tempFile());
+    // Add record
     dns.addItem(new AdresseIP("8.8.8.8"), new NomMachine("google.com"));
 
+    // Check  IP
     DnsItem item1 = dns.getItem(new AdresseIP("8.8.8.8"));
     assertEquals("google.com", item1.getNom().getNom());
 
+    // Check Nom
     DnsItem item2 = dns.getItem(new NomMachine("google.com"));
     assertEquals("8.8.8.8", item2.getAdresse().getAdresse());
   }
 
   @Test
   public void testGetItemsByDomain() throws Exception {
-    Dns dns = new Dns(tempFile());
+    // Add records
     dns.addItem(new AdresseIP("1.1.1.1"), new NomMachine("a.cloudflare.com"));
     dns.addItem(new AdresseIP("1.0.0.1"), new NomMachine("b.cloudflare.com"));
 
-    List<DnsItem> items = dns.getItems("cloudflare.com", true);
+    // Receive DNS
+    List<DnsItem> items = dns.getItems("cloudflare.com", false);
     assertEquals(2, items.size());
+
+    // Check sorting
+    assertEquals("a.cloudflare.com", items.get(0).getNom().getNom());
+    assertEquals("b.cloudflare.com", items.get(1).getNom().getNom());
   }
 }
+
